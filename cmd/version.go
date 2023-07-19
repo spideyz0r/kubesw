95de83e9596bd90ec242ccd47ded4e1cb2596a65
Breno Brand Fernandes <brandfbb@gmail.com>
2023-07-18 23:01:51 -0400

    Bump version, add version from cli

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	getVersion = &cobra.Command{
+		Use:   "version",
+		Short: "Get version of kubesw",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("v0.0.7")
+		},
+	}
+)
+
+func init() {
+	getCmd.AddCommand()
+}
